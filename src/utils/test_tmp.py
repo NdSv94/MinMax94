ea64e6ac998ed0b18385d5567f43970d67df96d2
NdSv94 <sviridenko.nadezda@phystech.edu>
2017-12-25 11:48:31 +0300

    Changed test for preprocessing

--- src/utils/test_tmp.py
+++ src/utils/test_tmp.py
@@ -10,7 +10,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # getting file tree in directory "data_csv", which contains raw unfiltered data
-mypath = '/mnt/HARD/MinMax94/data/data_all/CSV/Raw_extended/113_raw.csv'
+mypath = '/home/ndsviriden/data_csv/113_raw.csv'
 
 # reading loaded csv files from data_csv directory, output is a list (length=number of stations) of raw df
 raw_lmeteo = pd.read_csv(mypath, parse_dates = ['date_time'])
@@ -24,7 +24,6 @@ pattern_list = preprocessor.CreatePatternList(lmeteo_pivot)
 
 
 z = [pattern.date_time.max() - pattern.date_time.min() for pattern in pattern_list]
-print(max(z))
 
 
 
